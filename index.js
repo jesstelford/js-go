c9ab18672f9ebc3cd2d0cbb4edaf70d59a1e17c4
Jess Telford <jess+github@jes.st>
2016-07-12 18:37:02 +1000

    Initial AR with AFrame prototype

---
+++ index.js
@@ -0,0 +1,83 @@
+'use strict';
+
+var videoElement = document.querySelector('video');
+
+function getVideoBySource(videoSource) {
+  var constraints = {
+    video: {
+      deviceId: videoSource && videoSource.deviceId ? {exact: videoSource.deviceId} : undefined
+    }
+  };
+
+  return navigator.mediaDevices.getUserMedia(constraints)
+}
+
+function askPermission() {
+  return getVideoBySource();
+}
+
+function getDevices() {
+  return navigator.mediaDevices.enumerateDevices();
+}
+
+function getRearFacingVideoSource(sources) {
+
+  return getDevices().then(function (sources) {
+
+    var videoSources = sources.filter(source => source.kind === 'videoinput')
+
+    console.log('[video sources]:', videoSources);
+
+    if (!videoSources.length) {
+      throw new Error('Could not find any video sources');
+    }
+
+    var rearVideoSource;
+
+    videoSources.some(function(sourceInfo) {
+      var labelLower = sourceInfo.label.toLowerCase();
+      if (
+        labelLower.indexOf('back') !== -1
+        || labelLower.indexOf('environment') !== -1
+        || labelLower.indexOf('rear') !== -1
+      ) {
+        rearVideoSource = sourceInfo;
+        return true;
+      }
+      return false;
+    })
+
+    console.log('[rearVideoSource]:', rearVideoSource);
+
+    debugger;
+
+    return rearVideoSource || videoSources[0];
+
+  });
+
+}
+
+function getVideoStream(source) {
+  return getVideoBySource(source);
+}
+
+
+function gotStream(stream) {
+  window.stream = stream; // make stream available to console
+  videoElement.srcObject = stream;
+}
+
+function start() {
+  askPermission()
+    .then(getRearFacingVideoSource)
+    .then(getVideoStream)
+    .then(gotStream)
+    .catch(handleError);
+}
+
+start();
+
+function handleError(error) {
+  console.log('navigator.getUserMedia error: ', error);
+}
+
