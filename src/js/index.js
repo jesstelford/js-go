4fef668e25d641187b58508645a9767e683ecee4
Jess Telford <jess+github@jes.st>
2016-10-03 13:20:11 +1100

    Use click-drag component

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,51 @@
-// import stream from './video-stream';
 import aframe from 'aframe';
 import aframeExtras from 'aframe-extras';
-import draggable from './components/drag-move';
+import registerClickDragComponent from 'aframe-click-drag-component';
+import stream from './video-stream';
 
 require('webrtc-adapter');
 
 aframeExtras.physics.registerAll(aframe);
 aframeExtras.primitives.registerAll(aframe);
-draggable(aframe);
+aframe.registerComponent('hmd-controls', aframeExtras.controls['hmd-controls']);
+registerClickDragComponent(aframe);
 
-/*
-var videoElement = document.querySelector('video');
+const draggable = document.querySelector('[click-drag]');
+draggable.addEventListener('dragstart', _ => {
+  draggable.pause();
+});
+draggable.addEventListener('dragend', ({detail: {velocity}}) => {
 
-function gotStream(stream) {
-  window.stream = stream; // make stream available to console
-  videoElement.srcObject = stream;
+  // We're dealing with a very heavy ball (mass: 100), so we want to
+  // reduce the velocity a little
+  const velocityDamp = 0.3;
+
+  const camera = draggable.sceneEl.camera;
+
+  // The "up" vector of the camera
+  const rotation = camera.up.clone();
+  // crossed with the "forward" / direction vector of the camera
+  // gives us the 3rd orthogonal axis of the camera's rotation
+  rotation.cross(camera.getWorldDirection());
+
+  // A damped down velocity vector
+  const rotatedVelocity = new aframe.THREE.Vector3(
+    velocity.x * velocityDamp,
+    velocity.y * velocityDamp,
+    velocity.z * velocityDamp
+  );
+
+  rotatedVelocity.applyAxisAngle(rotation, Math.PI / 8);
+
+  draggable.play();
+  draggable.body.velocity.set(rotatedVelocity.x, rotatedVelocity.y, rotatedVelocity.z);
+});
+
+const videoElement = document.querySelector('video');
+
+function gotStream(videoStream) {
+  window.stream = videoStream; // make stream available to console
+  videoElement.srcObject = videoStream;
 }
 
 stream().then(gotStream);
-*/
--- src/js/video-stream.js
+++ src/js/video-stream.js
@@ -1,15 +1,11 @@
-'use strict';
-
-var videoElement = document.querySelector('video');
-
 function getVideoBySource(videoSource) {
-  var constraints = {
+  const constraints = {
     video: {
-      deviceId: videoSource && videoSource.deviceId ? {exact: videoSource.deviceId} : undefined
-    }
+      deviceId: videoSource && videoSource.deviceId ? {exact: videoSource.deviceId} : undefined,
+    },
   };
 
-  return navigator.mediaDevices.getUserMedia(constraints)
+  return navigator.mediaDevices.getUserMedia(constraints);
 }
 
 function askPermission() {
@@ -20,20 +16,20 @@ function getDevices() {
   return navigator.mediaDevices.enumerateDevices();
 }
 
-function getRearFacingVideoSource(sources) {
+function getRearFacingVideoSource() {
 
-  return getDevices().then(function (sources) {
+  return getDevices().then(sources => {
 
-    var videoSources = sources.filter(source => source.kind === 'videoinput')
+    const videoSources = sources.filter(source => source.kind === 'videoinput');
 
     if (!videoSources.length) {
       throw new Error('Could not find any video sources');
     }
 
-    var rearVideoSource;
+    let rearVideoSource;
 
     videoSources.some(function(sourceInfo) {
-      var labelLower = sourceInfo.label.toLowerCase();
+      const labelLower = sourceInfo.label.toLowerCase();
       if (
         labelLower.indexOf('back') !== -1
         || labelLower.indexOf('environment') !== -1
@@ -43,7 +39,7 @@ function getRearFacingVideoSource(sources) {
         return true;
       }
       return false;
-    })
+    });
 
     return rearVideoSource || videoSources[0];
 
@@ -55,14 +51,14 @@ function getVideoStream(source) {
   return getVideoBySource(source);
 }
 
+function handleError(error) {
+  // eslint-disable-next-line no-console
+  console.error('navigator.getUserMedia error: ', error);
+}
+
 export default function start() {
   return askPermission()
     .then(getRearFacingVideoSource)
     .then(getVideoStream)
     .catch(handleError);
 }
-
-function handleError(error) {
-  console.error('navigator.getUserMedia error: ', error);
-}
-
