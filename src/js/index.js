e9dd853ca08e8e725279fa1bf9a3b4f26c80a9eb
Jess Telford <jess+github@jes.st>
2016-11-07 22:17:14 +1100

    Add a map scene with real-time pins and GPS data

--- src/js/index.js
+++ src/js/index.js
@@ -6,8 +6,10 @@ import registerClickDragComponent from 'aframe-click-drag-component';
 import registerFrustumLockComponent from 'aframe-frustum-lock-component';
 import registerVideoBillboard from 'aframe-video-billboard';
 import aframeKeyboardControls from 'aframe-keyboard-controls';
+import registerMap from 'aframe-map';
 
 import CatchScene from './scenes/catch';
+import MapScene from './scenes/map';
 
 require('webrtc-adapter');
 
@@ -15,10 +17,10 @@ aframe.registerComponent('keyboard-controls', aframeKeyboardControls);
 registerClickDragComponent(aframe);
 registerFrustumLockComponent(aframe);
 registerVideoBillboard(aframe);
+registerMap(aframe);
 aframeExtras.physics.registerAll(aframe);
-aframeExtras.primitives.registerAll(aframe);
 aframeExtras.controls.registerAll(aframe);
 
 document.addEventListener('DOMContentLoaded', _ => {
-  ReactDOM.render(<CatchScene />, document.querySelector('#catch-scene'));
+  ReactDOM.render(<MapScene />, document.querySelector('#catch-scene'));
 });
